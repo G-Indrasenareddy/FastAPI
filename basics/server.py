580c054584302822dde46a46312804ace7d85cc5
Indrasena Reddy <indrasenareddygummireddy2005@gmail.com>
2026-07-14 09:30:56 +0530

    Initial FastAPI Project

---
+++ basics/server.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI
+
+app=FastAPI()
+
+'''
+Usage: Application Root Request
+Rest API URL: http://127.0.0.1:8000/
+Method Type:GET
+Required Fields: None 
+Acccess Type:Public
+'''
+@app.get("/")
+def index_page():
+    return {"msg":"Index Page"} 
+
+
+'''
+Usage: About Page
+Rest API URL: http://127.0.0.1:8000/about
+Method Type:GET
+Required Fields: None 
+Acccess Type:Public
+'''
+@app.get("/about")
+def about_page():
+    return {"msg":"About Page"}  
+
+'''
+Usage: Contact Page
+Rest API URL: http://127.0.0.1:8000/contact
+Method Type:GET
+Required Fields: None 
+Acccess Type:Public
+'''
+@app.get("/contact")
+def contact_page():
+    return {"msg":"Contact Page"} 
+
+
+'''
+Usage: Login Page
+Rest API URL: http://127.0.0.1:8000/login
+Method Type:GET
+Required Fields: None 
+Acccess Type:Public
+'''
+@app.get("/login")
+def login():
+    return {"msg":"Login Page"} 
+
+
+'''
+What is Virtual Env?
+How to active VENV?
+
+Install packages
+
+How to start server?
+uvicorn server:app --reload
+
+'''
